2393c188823dbe098cb5a352b863d6d2966460b7
Colt <austincgause@gmail.com>
2023-01-16 10:56:43 -0500

    Added files. Put sky, ground, & text.

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import pygame
 from sys import exit
+
 pygame.init()
-
-width = 800
-height = 400
-
-screen = pygame.display.set_mode((width,height)) # this sets the window size
-pygame.display.set_caption('Runner') # this changes the title of the window
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+testFont = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+skySurface = pygame.image.load('graphics/Sky.png')
+groundSurface = pygame.image.load('graphics/ground.png')
+textSurface = testFont.render('My game', False, 'Black')
 
 while True:
-
-    # inside this while loop, we are going to be drawing all elements and updating everything
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    '''
-    This event loop ensures that there is a way for pygame to close the window, and the sys exit() module
-    is better than just breaking the while loop, because it closes all the code at once. 
-    '''
+
+    screen.blit(skySurface, (0,0))
+    screen.blit(groundSurface, (0,300))
+    screen.blit(textSurface, (300,50))
 
     pygame.display.update()
     clock.tick(60) 
-    # this 60 is telling pygame that the while loop should not go more than 60 times a second
-
-
